c21d25a8cd41b751fec9def6222e0f5cc9f58224
BradRaynaud <bradraynaud@gmail.com>
2017-05-01 11:02:27 -0500

    Prototyping
    Brad Raynaud
    bmr023@latech.edu

--- Security_Pyramid.py
+++ Security_Pyramid.py
@@ -6,6 +6,9 @@
 #####################################################
 # Imports
 from time import sleep
+from Tkinter import *
+
+
 #####################################################
 # Room Class
 
@@ -34,18 +37,18 @@ class Room(object):
         self._exits = value
 
     # Generates the exits pass to it by the addExits function
-    def generateExit(self, Direction, Exists, Locked):
+    def generateExit(self, Direction, Exists, Locked, exitLocation):
         if Exists == True:
-            self.exits[Direction] = Locked
+            self.exits[Direction] = [Locked, exitLocation]
 
     # Assumes that rooms exist in all 4 directions and they are unlocked, arguments can be provided to remove exits
     # and lock exits
     def addExits(self, North=True, South=True, East=True, West=True, northLocked=False, southLocked=False,
-                 eastLocked=False, westLocked=False):
-        self.generateExit("North", North, northLocked)
-        self.generateExit("South", South, southLocked)
-        self.generateExit("East", East, eastLocked)
-        self.generateExit("West", West, westLocked)
+                 eastLocked=False, westLocked=False, nExit=None, sExit=None, eExit=None, wExit=None):
+        self.generateExit("North", North, northLocked, nExit)
+        self.generateExit("South", South, southLocked, sExit)
+        self.generateExit("East", East, eastLocked, eExit)
+        self.generateExit("West", West, westLocked, wExit)
 
     def __str__(self):
         s = "{} ".format(self.name)
@@ -54,18 +57,71 @@ class Room(object):
 
 
 #####################################################
-# Room Creation
+# Game Class
 
-Room1A = Room("Test_Room_1A")
-Room1A.addExits(northLocked=True, South=False)
-print Room1A
+class Game(Frame):
+    def __init(self, parent):
+        Frame.__init__(self, parent)
+
+    def createRooms(self):
+        Room1A = Room("Test_Room_1A")
+        Room1A.addExits(northLocked=True, South=False)
+
+        Game.currentRoom = Room1A
+
+    def setupGUI(self):
+        self.pack(fill=BOTH, expand=1)
+        Game.player_input = Entry(self, bg="white")
+        Game.player_input.bind("<Return>", self.process)
+        Game.player_input.pack(side=BOTTOM, fill=X)
+        Game.player_input.focus()
+
+    def status(self, response = None):
+        c = "you are in room {} and possible exits include {}, and {}".format(Game.currentRoom.name,
+                                                                      Game.currentRoom.exits.keys(), response)
+        print c
+
+    def play(self):
+        self.createRooms()
+        self.setupGUI()
+        self.status()
+
+    def process(self, event):
+        action = Game.player_input.get()
+        action = action.lower()
+
+        if (action == "quit" or action == "exit" or action == "bye"):
+            quit(0)
+
+        words = action.split()
+
+        response = "Try Again"
+
+        if len(words) == 2:
+            verb = words[0]
+            noun = words[1]
+
+        # the verb is go
+        if verb == "go":
+            response = "Invalid Exit"
+
+
+        self.status(response)
+
 
 ####################################################
 # Main Program
-timer = 0
-# 600 ticks equals about 1 minute
-while timer != 600:
-    timer += 1
-    sleep(.1)
+WIDTH = 800
+HEIGHT = 600
 
+# create the window
+window = Tk()
+window.title("Room Adventure")
 
+# create the GUI as a Tkinter canvas inside the window
+g = Game(window)
+# play the game
+g.play()
+
+# wait for the window to close
+window.mainloop()
