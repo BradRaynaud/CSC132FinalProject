955ae6f3eb1b9de6df3bc63a0d14db69a3383263
cdw085 <cdw085@latech.edu>
2017-05-03 15:52:18 -0500

    Here's the thing that should print a cardinal direction depending on which arrow is pressed.

--- Registering_Keystrokes.py
+++ Registering_Keystrokes.py
@@ -4,7 +4,19 @@ import pygame
 from time import sleep
 pygame.init()
 while True:
-    if pygame.key.get_focused() == True:
+    for event in pygame.event.get():
+        if event.type is KEYDOWN:
+            _ = pygame.key.name(event.key)
+            print _
+
+            if _ is "left":
+                print "west"
+            elif _ is "right":
+                print "east"
+            elif _ is "up":
+                print "north"
+            elif _ is "down":
+                print "south"
         key = pygame.key.get_pressed()
         print key
     sleep(4)
