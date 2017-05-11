098d5a1eeea3613b3fa31380ed9b76ac238a1c81
BradRaynaud <bradraynaud@gmail.com>
2017-05-10 21:47:17 -0500

    Created the start page
    Brad Raynaud
    bmr023@latech.edu

--- Menu_Testing.py
+++ Menu_Testing.py
@@ -18,36 +18,11 @@ panel4 = Label(root, textvariable=var2).grid(row=2, column=1, columnspan=1)
 var3 = StringVar()
 panel5 = Label(root, textvariable=var3).grid(row=2, column=2, columnspan=1)
 
-Button(root, text="Quit", command=lambda: show_frame("StartPage")).grid(row=2, column=4, columnspan=1)
+Button(root, text="Quit").grid(row=2, column=4, columnspan=1)
 Button(root, text="Quit").grid(row=2, column=5, columnspan=1)
 
 var.set("Score:0000(1)")
 var2.set("Score:0000(2)")
 var3.set("Score:0000(3)")
-
-
-class PageOne(Frame):
-    def __init__(self, parent, controller):
-        Frame.__init__(self, parent)
-        self.controller = controller
-        button = Button(self, text="Go to Main Menu",
-                        command=lambda: controller.show_frame("StartPage"))
-
-        button.pack()
-        # creates the paned window
-        m1 = PanedWindow(self)
-        m1.pack(fill=BOTH, expand=1)
-
-        left = Label(m1, text="Picture")
-        m1.add(left)
-
-        m2 = PanedWindow(m1, orient=VERTICAL)
-        m1.add(m2)
-
-        top = Label(m2, text="Answers")
-        m2.add(top)
-
-        bottom = Label(m2, text="Console")
-        m2.add(bottom)
 
 root.mainloop()
--- start.py
+++ start.py
@@ -1,78 +1,24 @@
+######################################################
+# This program is to provide the player with a title screen
+# The Start game button starts the Main_Game program
+# The Go to Scoreboard button starts the Score_Board program
+######################################################
+
 from Tkinter import *
-#needed for list on scoreboard
-import tkMessageBox
-import Tkinter
 
 TITLE_FONT = ("Helvetica", 18, "bold")
-LARGE_FONT= ("Verdana", 12)
-## * args is a list of arguments -as positional arguments
-## ** kwargs = dictionary - whose keys become separate keyword arguments and the values become values of these arguments. 
+LARGE_FONT = ("Verdana", 12)
 
-class SampleApp(Tk):
+window = Tk()
 
-    def __init__(self, *args, **kwargs):
-        Tk.__init__(self, *args, **kwargs)
+label = Label(text="This is the start page", font=TITLE_FONT)
+label.pack(side="top", fill="x", pady=10)
 
-        # the container is where we'll stack a bunch of frames
-        # on top of each other, then the one we want visible
-        # will be raised above the others
-        container = Frame(self)
-        container.pack(side="top", fill="both", expand=True)
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
+button1 = Button(text="Start Game")
 
-        self.frames = {}
-        for F in (StartPage, PageOne, PageTwo):
-            page_name = F.__name__
-            frame = F(parent=container, controller=self)
-            self.frames[page_name] = frame
+button2 = Button(text="Go to Scoreboard")
 
-            # put all of the pages in the same location;
-            # the one on the top of the stacking order
-            # will be the one that is visible.
-            frame.grid(row=0, column=0, sticky="nsew")
+button1.pack()
+button2.pack()
 
-        self.show_frame("StartPage")
-
-    def show_frame(self, page_name):
-        '''Show a frame for the given page name'''
-        frame = self.frames[page_name]
-        frame.tkraise()
-
-
-class StartPage(Frame):
-
-    def __init__(self, parent, controller):
-        Frame.__init__(self, parent)
-        self.controller = controller
-        label = Label(self, text="This is the start page", font=TITLE_FONT)
-        label.pack(side="top", fill="x", pady=10)
-
-        button1 = Button(self, text="Start Game",
-                            command=lambda: controller.show_frame("PageOne"))
-        
-        button2 = Button(self, text="Go to Scoreboard",
-                            command=lambda: controller.show_frame("PageTwo"))
-
-        button1.pack()
-        button2.pack()
-class PageOne(Frame):
-            
-    def __init__(self, parent, controller):
-        Frame.__init__(self, parent)
-        self.controller = controller
-        label = Label(self, text="place holder", font=LARGE_FONT)
-        label.pack(pady=10,padx=10)
-
-
-class PageTwo(Frame):
-            
-    def __init__(self, parent, controller):
-        Frame.__init__(self, parent)
-        self.controller = controller
-        label = Label(self, text="placeholder", font=LARGE_FONT)
-        label.pack(pady=10,padx=10)
-
-
-app = SampleApp()
-app.mainloop()
+window.mainloop()
