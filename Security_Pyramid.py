4da25a328c3410ce93d6907bcac20dfac0a12cbc
BradRaynaud <bradraynaud@gmail.com>
2017-05-11 21:08:56 -0500

    Exporting From my Surface Pro Brad

--- Security_Pyramid.py
+++ Security_Pyramid.py
@@ -26,7 +26,8 @@ QUESTIONMODE = False
 QUESTIONDICT = {}
 QUESTIONBANK = []
 DIRECTION = None
-
+LIVES = 3
+ATTEMPTS = 2
 
 #####################################################
 # Room Class
@@ -97,77 +98,190 @@ class Game(Frame):
     def __init(self, parent):
         Frame.__init__(self, parent)
 
+
     # Function generates the rooms
     def createRooms(self):
-        Room1A = Room("Test_Room_1A", "skull.gif")
-        Room2A = Room("Test_Room_2A", "skull.gif")
-        Room3A = Room("Test_Room_3A", "skull.gif")
-        Room4A = Room("Test_Room_4A", "skull.gif")
-        Room5A = Room("Test_Room_5A", "skull.gif")
-        Room6A = Room("Test_Room_6A", "skull.gif")
-        Room7A = Room("Test_Room_7A", "skull.gif")
-        Room8A = Room("Test_Room_8A", "skull.gif")
-        Room9A = Room("Test_Room_9A", "skull.gif")
-        Room1A.addExits(South=False, nExit=Room5A, eExit=Room2A, wExit=Room4A, northLocked=True)
-        Room2A.addExits(East=False, sExit=Room7A, nExit=Room6A, wExit=Room1A)
-        Room3A.addExits(North=False, South=False, eExit=Room7A, wExit=Room8A)
-        Room4A.addExits(West=False, eExit=Room1A, nExit=Room9A, sExit=Room8A)
-        Room5A.addExits(North=False, sExit=Room1A, eExit=Room6A, wExit=Room9A)
-        Room6A.addExits(North=False, East=False, wExit=Room5A, sExit=Room2A)
-        Room7A.addExits(South=False, East=False, nExit=Room2A, wExit=Room3A)
-        Room8A.addExits(South=False, West=False, eExit=Room3A, nExit=Room4A)
-        Room9A.addExits(West=False, North=False, sExit=Room4A, eExit=Room5A)
+        A1 = Room("Room A1")
+        A2 = Room("Room A2")
+        A3 = Room("Room A3")
+        A4 = Room("Room A4")
+        A5 = Room("Room A5")
+        A6 = Room("Room A6")
+        A7 = Room("Room A7")
+        A8 = Room("Room A8")
+
+        B1 = Room("Room B1")
+        B2 = Room("Room B2")
+        B3 = Room("Room B3")
+        B4 = Room("Room B4")
+        B5 = Room("Room B5")
+        B6 = Room("Room B6")
+        B7 = Room("Room B7")
+        B8 = Room("Room B8")
+
+        C1 = Room("Room C1")
+        C2 = Room("Room C2")
+        C3 = Room("Room C3")
+        C4 = Room("Room C4")
+        C5 = Room("Room C5")
+        C6 = Room("Room C6")
+        C7 = Room("Room C7")
+        C8 = Room("Room C8")
+
+        D1 = Room("Room D1 : Exit")
+        D2 = Room("Room D2")
+        D3 = Room("Room D3")
+        D4 = Room("Room D4")
+        D5 = Room("Room D5")
+        D6 = Room("Room D6")
+        D7 = Room("Room D7")
+        D8 = Room("Room D8")
+
+        E1 = Room("Room E1")
+        E2 = Room("Room E2")
+        E3 = Room("Room E3")
+        E4 = Room("Room E4")
+        E5 = Room("Room E5")
+        E6 = Room("Room E6")
+        E7 = Room("Room E7")
+        E8 = Room("Room E8 : Entrance")
+
+        F1 = Room("Room F1")
+        F2 = Room("Room F2")
+        F3 = Room("Room F3")
+        F4 = Room("Room F4")
+        F5 = Room("Room F5")
+        F6 = Room("Room F6")
+        F7 = Room("Room F7")
+        F8 = Room("Room F8")
+
+        G1 = Room("Room G1")
+        G2 = Room("Room G2")
+        G3 = Room("Room G3")
+        G4 = Room("Room G4")
+        G5 = Room("Room G5")
+        G6 = Room("Room G6")
+        G7 = Room("Room G7")
+        G8 = Room("Room G8")
+
+        H1 = Room("Room H1")
+        H2 = Room("Room H2")
+        H3 = Room("Room H3")
+        H4 = Room("Room H4")
+        H5 = Room("Room H5")
+        H6 = Room("Room H6")
+        H7 = Room("Room H7")
+        H8 = Room("Room H8")
+
+        A1.addExits(eExit=B1, sExit=A2, North=False, West=False)
+        A2.addExits(nExit=A1, eExit=B2, South=False, West=False)
+        A3.addExits(eExit=B3, sExit=A4, North=False, West=False, southLocked=True)
+        A4.addExits(nExit=A3, South=False, East=False, West=False)
+        A5.addExits(eExit=B5, sExit=A6, North=False, West=False)
+        A6.addExits(nExit=A5, eExit=B6, South=False, West=False)
+        A7.addExits(eExit=B7, sExit=A8, North=False, West=False)
+        A8.addExits(nExit=A7, eExit=B8, South=False, West=False, northLocked=True)
+
+        B1.addExits(wExit=A1, North=False, South=False, East=False)
+        B2.addExits(wExit=A2, sExit=B3, North=False, East=False, westLocked=True)
+        B3.addExits(nExit=B2, wExit=A3, sExit=B4, East=False)
+        B4.addExits(sExit=B5, nExit=B3, East=False, West=False)
+        B5.addExits(nExit=B4, wExit=A5, East=False, South=False, northLocked=True)
+        B6.addExits(wExit=A6, sExit=B7, North=False, East=False, westLocked=True)
+        B7.addExits(nExit=B6, wExit=A7, East=False, South=False)
+        B8.addExits(wExit=A8, eExit=C8, North=False, South=False)
+
+        C1.addExits(eExit=D1, sExit=C2, North=False, West=False, eastLocked=True)
+        C2.addExits(nExit=C1, sExit=C3, East=False, West=False, northLocked=True)
+        C3.addExits(nExit=C2, sExit=C4, East=False, West=False, northLocked=True)
+        C4.addExits(nExit=C3, sExit=C5, West=False, East=False)
+        C5.addExits(nExit=C4, eExit=D5, South=False, West=False, northLocked=True)
+        C6.addExits(eExit=C5, sExit=C7, North=False, West=False)
+        C7.addExits(nExit=C6, eExit=D7, South=False, West=False)
+        C8.addExits(eExit=D8, wExit=B8, North=False, South=False, westLocked=True)
+
+        D1.addExits(wExit=C1, North=False, East=False, South=False)
+        D2.addExits(eExit=E2, North=False, South=False, West=False)
+        D3.addExits(eExit=E3, sExit=D4, North=False, West=False)
+        D4.addExits(nExit=D3, eExit=E4, South=False, West=False, eastLocked=True)
+        D5.addExits(wExit=C5, eExit=E5, North=False, South=False, westLocked=True)
+        D6.addExits(wExit=C6, eExit=E6, North=False, South=False, eastLocked=True)
+        D7.addExits(wExit=C7, eExit=E7, sExit=D8, North=False, westLocked=True, southLocked=True)
+        D8.addExits(wExit=C8, eExit=E8, North=False, South=False)
+
+        E1.addExits(eExit=F1, sExit=E2, North=False, West=False, eastLocked=True)
+        E2.addExits(nExit=E1, eExit=F2, wExit=D2, South=False, westLocked=True)
+        E3.addExits(wExit=D3, eExit=F3, North=False, South=False, westLocked=True)
+        E4.addExits(wExit=D4, North=False, South=False, East=False)
+        E5.addExits(wExit=D5, sExit=E6, North=False, East=False)
+        E6.addExits(nExit=E5, wExit=D6, East=False, South=False)
+        E7.addExits(wExit=D7, eExit=F7, North=False, South=False)
+        E8.addExits(eExit=F8, North=False, South=False, West=False, eastLocked=True)
+
+        F1.addExits(wExit=E1, North=False, South=False, East=False)
+        F2.addExits(wExit=E2, eExit=G2, North=False, South=False)
+        F3.addExits(wExit=E3, eExit=G3, sExit=F4, North=False, eastLocked=True)
+        F4.addExits(nExit=F3, eExit=G4, sExit=F5, West=False)
+        F5.addExits(nExit=F4, sExit=F6, East=False, West=False, southLocked=True)
+        F6.addExits(nExit=F5, sExit=F7, East=False, West=False)
+        F7.addExits(nExit=F6, wExit=E7, East=False, South=False, westLocked=True)
+        F8.addExits(wExit=E8, eExit=G8, South=False, North=False)
+
+        G1.addExits(sExit=G2, eExit=H1, North=False, West=False)
+        G2.addExits(wExit=F2, nExit=G1, East=False, South=False, westLocked=True)
+        G3.addExits(wExit=F3, North=False, South=False, East=False)
+        G4.addExits(wExit=F4, eExit=H4, North=False, South=False, westLocked=True)
+        G5.addExits(eExit=H5, sExit=G6, North=False, West=False, eastLocked=True)
+        G6.addExits(nExit=G5, sExit=G7, East=False, South=False)
+        G7.addExits(nExit=G6, sExit=G8, eExit=H7, West=False, northLocked=True)
+        G8.addExits(wExit=F8, nExit=G7, East=False, South=False, northLocked=True)
+
+        H1.addExits(wExit=G1, sExit=H2, North=False, East=False)
+        H2.addExits(nExit=H1, sExit=H3, East=False, West=False, northLocked=True)
+        H3.addExits(nExit=H2, sExit=H4, East=False, West=False)
+        H4.addExits(nExit=H3, sExit=H5, wExit=G4, East=False, westLocked=True)
+        H5.addExits(nExit=H4, sExit=H6, wExit=G5, East=False, northLocked=True, southLocked=True)
+        H6.addExits(nExit=H5, East=False, South=False, West=False)
+        H7.addExits(wExit=G7, sExit=H8, North=False, East=False, southLocked=True)
+        H8.addExits(nExit=H7, East=False, South=False, West=False)
 
 
-        Game.currentRoom = Room1A
-        Game.Score = 0
+        Game.currentRoom = D1
 
     # Function creates and formats the GUI
-    def setupGUI(self):
-        # Organize the GUI
-        self.pack(fill=BOTH, expand=1)
+    def setupGUI(self, i):
+        if i == 0:
+            # Organize the GUI
+            self.pack(fill=BOTH, expand=1)
 
-        # setup the image to the left of the GUI
-        # the widget is a TKinter label
-        # don't let the image control the widget size
-        img = None
+            # setup the image to the left of the GUI
+            # the widget is a TKinter label
+            # don't let the image control the widget size
+            img = None
 
-        Game.image = Label(self, width=WIDTH / 2, image=img)
-        Game.image.image = img
-        Game.image.grid(row=0, column=0)
-        Game.image.pack_propagate(False)
+            Game.image = Label(self, width=WIDTH / 2, image=img)
+            Game.image.image = img
+            Game.image.grid(row=0, column=0)
+            Game.image.pack_propagate(False)
 
-        Game.img = PhotoImage(file="skull.gif")
-        Game.image.config(image=Game.img)
-        Game.image.image = Game.img
+            Game.img = PhotoImage(file="skull.gif")
+            Game.image.config(image=Game.img)
+            Game.image.image = Game.img
 
-        # setup the text to the right of the GUI
-        # first the frame in which the text will be placed
-        text_frame = Frame(self, width=WIDTH / 4)
-        # widget is a TKINTER Text
-        # disable it by default
-        # don't let the widget control the frame's size
-        Game.text = Text(text_frame, bg="lightgrey", state=DISABLED)
-        Game.text.grid(row=0, column=2)
-        text_frame.grid(row=0, column=2)
-        text_frame.pack_propagate(False)
+            # setup the text to the right of the GUI
+            # first the frame in which the text will be placed
+            text_frame = Frame(self, width=WIDTH / 4)
+            # widget is a TKINTER Text
+            # disable it by default
+            # don't let the widget control the frame's size
+            Game.text = Text(text_frame, bg="lightgrey", state=DISABLED)
+            Game.text.grid(row=0, column=2)
+            text_frame.grid(row=0, column=2)
+            text_frame.pack_propagate(False)
 
-        # menu bar at bottom
-        var = StringVar()
-        panel3 = Label(self, textvariable=var).grid(row=2, column=0, columnspan=1)
+            Button(self, text="Quit").grid(row=2, column=4, columnspan=1)
+            Button(self, text="Quit").grid(row=2, column=5, columnspan=1)
 
-        var2 = StringVar()
-        panel4 = Label(self, textvariable=var2).grid(row=2, column=1, columnspan=1)
-
-        var3 = StringVar()
-        panel5 = Label(self, textvariable=var3).grid(row=2, column=2, columnspan=1)
-
-        Button(self, text="Quit").grid(row=2, column=4, columnspan=1)
-        Button(self, text="Quit").grid(row=2, column=5, columnspan=1)
-
-        var.set("Score:0000(1)")
-        var2.set("Score:0000(2)")
-        var3.set("Score:0000(3)")
 
     # exports the status to the GUI
     def status(self, response=None):
@@ -176,12 +290,12 @@ class Game(Frame):
         Game.text.delete("1.0", END)
         if response == None:
             response = "Welcome to the Labyrinth."
-        if (Game.currentRoom.name == "Test_Room_7A"):
+        if (Game.currentRoom.name == "Room D1 : Exit"):
             # if the end is found let the player know
             response = "Congratulations you have found the exit"
 
         # otherwise, display the appropriate status
-        Game.text.insert(END, str(Game.currentRoom) + "\n\n" + response)
+        Game.text.insert(END, str(Game.currentRoom) +"\nScore:{}\nLives:{}\nAttempts Remaining:{}".format(SCORE,LIVES,ATTEMPTS) + "\n\n" + response)
         Game.text.config(state=DISABLED)
 
     # exports a status to the GUI when the player is asked a question
@@ -199,7 +313,7 @@ class Game(Frame):
     # function that starts the GUI and plays the game
     def play(self):
         self.createRooms()
-        self.setupGUI()
+        self.setupGUI(0)
         self.status()
 
     # function that moves the player from room to room
@@ -211,7 +325,6 @@ class Game(Frame):
             if Game.currentRoom.exits[direction][0] == False:
                 response = "Room Changed"
                 Game.currentRoom = Game.currentRoom.exits[direction][1]
-                self.Score += 1
                 self.status(response)
             elif Game.currentRoom.exits[direction][0] == True:
                 self.askQuestion()
@@ -223,7 +336,8 @@ class Game(Frame):
         global TEMPQUESTION
         QNAME = selectQuestion()
         TEMPQUESTION = QUESTIONDICT[QNAME]
-        response = "{}\n{}\n{}\n{}".format(QNAME, TEMPQUESTION[0][0], TEMPQUESTION[1][0], TEMPQUESTION[2][0], TEMPQUESTION[3][0])
+        print TEMPQUESTION
+        response = "{}\n{}\n{}\n{}\n{}".format(QNAME, TEMPQUESTION[0][0], TEMPQUESTION[1][0], TEMPQUESTION[2][0], TEMPQUESTION[3][0])
         self.questionStatus(response)
         question()
 
@@ -349,7 +463,7 @@ HEIGHT = 600
 
 # create the window
 window = Tk()
-window.title("Room Adventure")
+window.title("Maze Gate")
 
 # create the GUI as a Tkinter canvas inside the window
 g = Game(window)
